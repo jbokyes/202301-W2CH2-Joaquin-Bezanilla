5a1176139034dc286997924c858b0660c0500a2f
jbezanilla <joaquinbezanilla@gmail.com>
2023-01-24 17:57:16 +0100

    fixed eslint suggestion

--- strictEquals.js
+++ strictEquals.js
@@ -12,16 +12,7 @@ export const strictEquals = (a, b) => {
 
   if (Object.is(a, b)) {
     return true;
-  } else {
-    return false;
   }
-};
 
-console.log(strictEquals(1, 1));
-console.log(strictEquals(NaN, NaN));
-console.log(strictEquals(0, -0));
-console.log(strictEquals(-0, 0));
-console.log(strictEquals(1, "1"));
-console.log(strictEquals(true, false));
-console.log(strictEquals(false, false));
-console.log(strictEquals("water", "oil"));
+  return false;
+};
